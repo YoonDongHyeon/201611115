436d580fd14dc9a410ba6e140ed7ed5e867a530d
YoonDongHyeon <dhdh9595@naver.com>
2016-11-15 20:05:09 +0900

    20161115 수업내용

--- 20161115_2.py
+++ 20161115_2.py
@@ -6,3 +6,10 @@ import class20161115
 print(class20161115.myadd(10,15))
 print(class20161115,mysub(10,15))
 # 패키지를 만들면 나의 패키지에서 항상 D드라이브에서 원하는 모듈을 불러올 수 있다.
+#pypi에 가보면 필요한 패키지들을 찾아서 사용할 수 있다.
+# setup 파일로 설치, test파일,example파일로 미리 돌려볼 수 있다.
+# 실행창에서 whl파일 설치 방법은
+# python -m pipi install U pip 혹은
+# python -m pip install 파일이름으로 설치 해줄 수 있다.
+# www.lfd.uci.edu/~gohlke/pythonlibs/ 에서 whl파일로 패키지들을 다운 받을 수 있다.
+# xlwt xlrd numpy matplotlib 패키지는 다운받아놔라
